0e9abafbed452bee1e11189b226fa524429c5fd4
Sebastián <thelopezepol@outlook.es>
2021-05-16 17:59:50 -0600

    queries

--- JavaIndexer/src/main/Main.java
+++ JavaIndexer/src/main/Main.java
@@ -5,33 +5,12 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String query = "ref:\"fiebre oro\"~2 AND ref:\"rio nilo\"~2";
-
-        String[] terms = query.split("\"", 0);
-
-        for (String term : terms) {
-            System.out.println(term);
-        }
-
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-        query = "ref:fiebre AND ref:oro";
+        String word = "cuanacaste~0.8";
+        int sep = word.indexOf('~');
 
-        terms = query.split(" ", 0);
-
-        for (String term : terms) {
-            System.out.println(term);
-        }
-
-        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-        query = "fiebre^1 amarilla^4";
-
-        terms = query.split(" ", 0);
-
-        for (String term : terms) {
-            System.out.println(term);
-        }
+        System.out.println(word.substring(sep + 1));
 
         /*ArchiveManager index = new ArchiveManager();
         index.index_HTMLS("Asia");
--- JavaIndexer/src/queries/Query.java
+++ JavaIndexer/src/queries/Query.java
@@ -1,37 +1,99 @@
 package queries;
 
-public class Query implements StringManager{
-    private Term[] terminos;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+public class Query implements StringManager {
     public int size;
+    private Term[] terminos;
 
     Query() {
-        this.size = 0;
-        this.terminos = new Term[size];
-    }
-
-    public void setTerminos(String query) {
-        String[] sentence;
-        if (query.contains("\"")) {
-            sentence = query.split("\"");
-            sentence = StringManager.removeElements(sentence, "");
-            setTermsWithQuotes(sentence);
-        }
-        else {
-            sentence = query.split(" ");
-            sentence = StringManager.removeElements(sentence, "");
-            setTermsWithKeys(sentence);
-        }
+        this.size = -1;
+        this.terminos = new Term[0];
     }
 
     public Term[] getTerminos() {
         return terminos;
     }
 
-    private void setTermsWithQuotes(String[] sentence) {
+    public void setTerminos(@NotNull String query) {
+        String[] sentence;
+        if (query.contains("\"")) {
+            sentence = query.split("\"");
+            sentence = StringManager.removeElements(sentence, "");
+            setTermsWithQuotes(sentence);
+        } else {
+            sentence = query.split(" ");
+            sentence = StringManager.removeElements(sentence, "");
+            setTermsWithKeys(sentence);
+        }
+    }
 
+    private void setTermsWithQuotes(String[] sentence) {
+        Term term = new Term();
+        for (String s : sentence) {
+            String[] words = s.split(" ", 0);
+            boolean hasTerm = false;
+            for (String word : words) {
+                if (word.equals("ref:"))
+                    term.isRef = true;
+                else if (word.contains("~"))
+                    term.distanceWithTerm = Integer.parseInt(word.substring(word.length() - 1));
+                else if (!hasTerm) {
+                    term.keyword = word;
+                    term.distanceWithTerm = 1;
+                    hasTerm = true;
+                    appendTerm(term);
+                } else {
+                    term.associatedTerm = new Term(word,
+                            term.isRef,
+                            term.priority,
+                            term.pctError,
+                            term.typeOfBooleanSearch,
+                            term.distanceWithTerm,
+                            term);
+                    appendTerm(term.associatedTerm);
+                }
+
+            }
+        }
     }
 
     private void setTermsWithKeys(String[] sentence) {
+        for (String word : sentence) {
+            int sep = 0;
+            Term term = new Term();
+            boolean haveToAppend = true;
 
+            if (word.contains("ref:")) {
+                sep = word.indexOf(':');
+                term.keyword = word.substring(sep + 1);
+                term.isRef = true;
+            } else if (word.contains("^")) {
+                sep = word.indexOf('^');
+                term.keyword = word.substring(0, sep);
+                term.priority = Integer.parseInt(word.substring(sep + 1));
+            } else if (word.contains("~")) {
+                sep = word.indexOf('~');
+                term.keyword = word.substring(0, sep);
+                term.pctError = Double.parseDouble(word.substring(sep + 1));
+            } else if (word.equals("AND") && this.size > -1) {
+                this.terminos[size - 1].typeOfBooleanSearch = BooleanSearch.AND;
+                haveToAppend = false;
+            } else if (word.equals("OR") && this.size > -1) {
+                this.terminos[size - 1].typeOfBooleanSearch = BooleanSearch.OR;
+                haveToAppend = false;
+            } else
+                term.keyword = word;
+
+            if (haveToAppend)
+                appendTerm(term);
+        }
+    }
+
+    private void appendTerm(Term term) {
+        this.terminos = Arrays.copyOf(this.terminos, ++size);
+        this.terminos[size] = term;
     }
 }
