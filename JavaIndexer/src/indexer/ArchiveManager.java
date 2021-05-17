424efd543a96112e13043561bcaef9c808db7eca
revv-tech <52094428+revv-tech@users.noreply.github.com>
2021-05-17 00:16:54 -0600

    Stemming Listo

--- JavaIndexer/src/indexer/ArchiveManager.java
+++ JavaIndexer/src/indexer/ArchiveManager.java
@@ -1,13 +1,14 @@
 package indexer;
 
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.PorterStemFilter;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
-
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-
+import org.apache.lucene.index.*;
 import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
@@ -16,10 +17,9 @@ import org.apache.lucene.util.Version;
 import org.jsoup.Jsoup;
 
 
-
 import java.io.*;
-import java.util.Scanner;
-
+import java.text.Normalizer;
+import java.util.*;
 
 
 public class ArchiveManager {
@@ -28,56 +28,425 @@ public class ArchiveManager {
 
     // C:\Users\Marco\Desktop\Documentos TEC\GeoIndexer\JavaIndexer\IndexConsult
     // C:\Users\Marco\Desktop\Documentos TEC\GeoIndexer\JavaIndexer\stopwords.txt
-    StandardAnalyzer analyzer;
+    Analyzer analyzer;
     Directory index;
     File indexDirectoryPath = new File("C:\\Users\\Marco\\Desktop\\Documentos TEC\\GeoIndexer\\JavaIndexer\\IndexConsult");
     IndexWriterConfig config;
     IndexWriter writer;
-    int count = 0;
-    static File stopwords = new File("C:\\Users\\Marco\\Desktop\\Documentos TEC\\GeoIndexer\\JavaIndexer\\stopwords.txt");
+    String stopwords[] = {
+            "a",
+            "acá",
+            "ahí",
+            "ajena",
+            "ajenas",
+            "ajeno",
+            "ajenos",
+            "al",
+            "algo",
+            "algún",
+            "alguna",
+            "algunas",
+            "alguno",
+            "algunos",
+            "allá",
+            "alli",
+            "allí",
+            "ambos",
+            "ampleamos",
+            "ante",
+            "antes",
+            "aquel",
+            "aquella",
+            "aquellas",
+            "aquello",
+            "aquellos",
+            "aqui",
+            "aquí",
+            "arriba",
+            "asi",
+            "atras",
+            "aun",
+            "aunque",
+            "bajo",
+            "bastante",
+            "bien",
+            "cabe",
+            "cada",
+            "casi",
+            "cierta",
+            "ciertas",
+            "cierto",
+            "ciertos",
+            "como",
+            "cómo",
+            "con",
+            "conmigo",
+            "conseguimos",
+            "conseguir",
+            "consigo",
+            "consigue",
+            "consiguen",
+            "consigues",
+            "contigo",
+            "contra",
+            "cual",
+            "cuales",
+            "cualquier",
+            "cualquiera",
+            "cualquieras",
+            "cuan",
+            "cuán",
+            "cuando",
+            "cuanta",
+            "cuánta",
+            "cuantas",
+            "cuántas",
+            "cuanto",
+            "cuánto",
+            "cuantos",
+            "cuántos",
+            "de",
+            "dejar",
+            "del",
+            "demás",
+            "demas",
+            "demasiada",
+            "demasiadas",
+            "demasiado",
+            "demasiados",
+            "dentro",
+            "desde",
+            "donde",
+            "dos",
+            "el",
+            "él",
+            "ella",
+            "ellas",
+            "ello",
+            "ellos",
+            "empleais",
+            "emplean",
+            "emplear",
+            "empleas",
+            "empleo",
+            "en",
+            "encima",
+            "entonces",
+            "entre",
+            "era",
+            "eramos",
+            "eran",
+            "eras",
+            "eres",
+            "es",
+            "esa",
+            "esas",
+            "ese",
+            "eso",
+            "esos",
+            "esta",
+            "estaba",
+            "estado",
+            "estais",
+            "estamos",
+            "estan",
+            "estar",
+            "estas",
+            "este",
+            "esto",
+            "estos",
+            "estoy",
+            "etc",
+            "fin",
+            "fue",
+            "fueron",
+            "fui",
+            "fuimos",
+            "gueno",
+            "ha",
+            "hace",
+            "haceis",
+            "hacemos",
+            "hacen",
+            "hacer",
+            "haces",
+            "hacia",
+            "hago",
+            "hasta",
+            "incluso",
+            "intenta",
+            "intentais",
+            "intentamos",
+            "intentan",
+            "intentar",
+            "intentas",
+            "intento",
+            "ir",
+            "jamás",
+            "junto",
+            "juntos",
+            "la",
+            "largo",
+            "las",
+            "lo",
+            "los",
+            "mas",
+            "más",
+            "me",
+            "menos",
+            "mi",
+            "mía",
+            "mia",
+            "mias",
+            "mientras",
+            "mio",
+            "mío",
+            "mios",
+            "mis",
+            "misma",
+            "mismas",
+            "mismo",
+            "mismos",
+            "modo",
+            "mucha",
+            "muchas",
+            "muchísima",
+            "muchísimas",
+            "muchísimo",
+            "muchísimos",
+            "mucho",
+            "muchos",
+            "muy",
+            "nada",
+            "ni",
+            "ningun",
+            "ninguna",
+            "ningunas",
+            "ninguno",
+            "ningunos",
+            "no",
+            "nos",
+            "nosotras",
+            "nosotros",
+            "nuestra",
+            "nuestras",
+            "nuestro",
+            "nuestros",
+            "nunca",
+            "os",
+            "otra",
+            "otras",
+            "otro",
+            "otros",
+            "para",
+            "parecer",
+            "pero",
+            "poca",
+            "pocas",
+            "poco",
+            "pocos",
+            "podeis",
+            "podemos",
+            "poder",
+            "podria",
+            "podriais",
+            "podriamos",
+            "podrian",
+            "podrias",
+            "por",
+            "por qué",
+            "porque",
+            "primero",
+            "puede",
+            "pueden",
+            "puedo",
+            "pues",
+            "que",
+            "qué",
+            "querer",
+            "quien",
+            "quién",
+            "quienes",
+            "quienesquiera",
+            "quienquiera",
+            "quiza",
+            "quizas",
+            "sabe",
+            "sabeis",
+            "sabemos",
+            "saben",
+            "saber",
+            "sabes",
+            "se",
+            "segun",
+            "ser",
+            "si",
+            "sí",
+            "siempre",
+            "siendo",
+            "sin",
+            "sín",
+            "sino",
+            "so",
+            "sobre",
+            "sois",
+            "solamente",
+            "solo",
+            "somos",
+            "soy",
+            "sr",
+            "sra",
+            "sres",
+            "sta",
+            "su",
+            "sus",
+            "suya",
+            "suyas",
+            "suyo",
+            "suyos",
+            "tal",
+            "tales",
+            "también",
+            "tambien",
+            "tampoco",
+            "tan",
+            "tanta",
+            "tantas",
+            "tanto",
+            "tantos",
+            "te",
+            "teneis",
+            "tenemos",
+            "tener",
+            "tengo",
+            "ti",
+            "tiempo",
+            "tiene",
+            "tienen",
+            "toda",
+            "todas",
+            "todo",
+            "todos",
+            "tomar",
+            "trabaja",
+            "trabajais",
+            "trabajamos",
+            "trabajan",
+            "trabajar",
+            "trabajas",
+            "trabajo",
+            "tras",
+            "tú",
+            "tu",
+            "tus",
+            "tuya",
+            "tuyo",
+            "tuyos",
+            "ultimo",
+            "un",
+            "una",
+            "unas",
+            "uno",
+            "unos",
+            "usa",
+            "usais",
+            "usamos",
+            "usan",
+            "usar",
+            "usas",
+            "uso",
+            "usted",
+            "ustedes",
+            "va",
+            "vais",
+            "valor",
+            "vamos",
+            "van",
+            "varias",
+            "varios",
+            "vaya",
+            "verdad",
+            "verdadera",
+            "vosotras",
+            "vosotros",
+            "voy",
+            "vuestra",
+            "vuestras",
+            "vuestro",
+            "vuestros",
+            "y",
+            "ya",
+            "yo"
+    };
+    ArrayList<String> directoriosAgregados = new ArrayList<String>();
 
-    public void ArchiveManager() throws IOException{
-    }
+    public ArchiveManager() throws IOException{}
+
     //Indexa HTMLs de acuerdo al continente dado
-    public void index_HTMLS(String continent) throws IOException {
-        //Crea analizador NECESARIO PARA LUCENE BUSQUEDAR E INDEXAR
-        analyzer = new StandardAnalyzer(Version.LUCENE_36);
+    public void creator_() throws IOException {
+        //Crea analizador NECESARIO PARA LUCENE BUSQUEDAR E INDEXAR con stopwords
+
+        analyzer = new MyAnalyzer();
         //Lugar en donde se almacena los docs del index
         index = FSDirectory.open(indexDirectoryPath);
         //Escritor del index
-        config = new IndexWriterConfig(Version.LUCENE_36,analyzer);
+        config = new IndexWriterConfig(Version.LUCENE_36,analyzer).setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
         writer = new IndexWriter(index, analyzer,true, new IndexWriter.MaxFieldLength(25000));
-        //Agrega los files al index
-        addFiles(new File("C:\\Users\\Marco\\Desktop\\Documentos TEC\\GeoIndexer\\JavaIndexer\\Geografia\\" + continent));
-        System.out.println("RAM kilobytes used: " + this.writer.ramSizeInBytes()/1000);
-        writer.close();
+
     }
 
 
+    public void index_Files(String continent) throws IOException {
+
+        if (!directoriosAgregados.contains(continent)) {
+            directoriosAgregados.add(continent);
+            // Agrega los files al index
+            addFiles(new File("C:\\Users\\Marco\\Desktop\\Documentos TEC\\GeoIndexer\\JavaIndexer\\Geografia\\" + continent),writer);
+
+        }
+        else{
+            IndexWriter newIndex = new IndexWriter(index, writer.getConfig());
+            addFiles(new File("C:\\Users\\Marco\\Desktop\\Documentos TEC\\GeoIndexer\\JavaIndexer\\Geografia\\" + continent),newIndex);
+
+        }
+        for (int i = 0; i < directoriosAgregados.size(); ++i)
+            System.out.println(directoriosAgregados.get(i) + " ha sido indexado!");
+
+    }
+
 
     //Agrega el archivo al index
-    public void addFiles(File file) throws IOException {
+    public void addFiles(File file, IndexWriter writerP) throws IOException {
+
         //Verifica si es directorio
+
         if (file.isDirectory()) {
+
             //Lo indexa
             System.out.println("Indexando " + file.getName() + "...........");
             File[] fileList = file.listFiles();
+
             for (File f : fileList) {
-                addFiles(f);
+                addFiles(f,writerP);
                 System.out.println(f.getName() + " ha sido agregado al index!");
             }
+
         }
         //Si es html lo indexa
         else if (!file.isHidden() && file.exists() && file.canRead()) {
+
             //make doc
             Document doc = getDocument(file);
             try {
                 //add doc to writer
-                writer.addDocument(doc);
+                writerP.addDocument(doc);
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
         }
+
 
     }
 
@@ -89,15 +458,33 @@ public class ArchiveManager {
             //Parse file and add body and title to doc
             org.jsoup.nodes.Document soupDoc = Jsoup.parse(file, "UTF-8");
             //ETIQUETAS VERIFICAR CUALES
-            String body = soupDoc.getElementsByTag("body").text();
-            String title = soupDoc.getElementsByTag("title").text();
-            String a = soupDoc.getElementsByTag("a").text();
-            System.out.println(title);
+
+            //Titulo
+            String title = soupDoc.getElementsByTag("title").text().toLowerCase();
+            //Resumen
+            String summary = soupDoc.getElementsByTag("p").text().substring(0,200);
+            //Texto
+            String texto = soupDoc.getElementsByTag("p").text().toLowerCase();
+            //Normalizacion
+            texto = Normalizer.normalize(texto,Normalizer.Form.NFKD);
+            texto = texto.replaceAll("[^\\p{ASCII}]", "");
+            texto = removeStopWordsAndStem(texto);
+            //System.out.println(texto);
+            //Ref
+            String a = soupDoc.getElementsByTag("a").text().toLowerCase().toLowerCase();
+            //Control de vocales en Ref
+            a = a.replaceAll("[\b?á\b?]", "");
+            a = a.replaceAll("[\b?í\b?]", "");
+            a = a.replaceAll("[\b?ó\b?]", "");
+            a = a.replaceAll("[\b?ú\b?]", "");
+            a = a.replaceAll("[\b?é\b?]", "");
+            //System.out.println(a);
             //AGREGAR ETIQUETAS
-            addDocument(doc, "body", body, 1);
             addDocument(doc, "title", title, 2);
+            addDocument(doc, "resumen", summary, 1);
             addDocument(doc, "url", file.getAbsolutePath(), 1);
-            addDocument(doc, "a", a, 1.5f);
+            addDocument(doc, "ref", a, 1.5f);
+            addDocument(doc,"texto",texto,1);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -121,16 +508,15 @@ public class ArchiveManager {
         System.out.print("Introduzca su consulta: ");
         Scanner s = new Scanner(System.in);
         String querystr = s.nextLine();
+
         try {
             //Create Query
-            Query q = new MultiFieldQueryParser(Version.LUCENE_36,new String[] {"title", "body", "a"}, analyzer).parse(querystr);
+            Query q = new MultiFieldQueryParser(Version.LUCENE_36,new String[] {"texto"}, analyzer).parse(querystr);
 			System.out.println(q.toString());
             //Create Lucene searcher
             Directory indexDirectory = FSDirectory.open(indexDirectoryPath);
             IndexReader r = IndexReader.open(indexDirectory);
             IndexSearcher searcher = new IndexSearcher(r);
-
-            //Get top 5 docs from search results
 
             TopDocs retrievedDocs = searcher.search(q,null,20);
 
@@ -164,7 +550,28 @@ public class ArchiveManager {
         if (querystr.trim().toLowerCase().equals("y")) {
             searchQuery();
         }
-        s.close();
+
+    }
+
+    public void closeWriter() throws IOException {
+        writer.close();
+    }
+
+    //Stemming
+    public String removeStopWordsAndStem(String input) throws IOException {
+        TokenStream tokenStream = new StandardTokenizer(
+                Version.LUCENE_36, new StringReader(input));
+        tokenStream = new StopFilter(true, tokenStream, Collections.singleton(stopwords));
+        tokenStream = new PorterStemFilter(tokenStream);
+        StringBuilder sb = new StringBuilder();
+        TermAttribute termAttr = tokenStream.getAttribute(TermAttribute.class);
+        while (tokenStream.incrementToken()) {
+            if (sb.length() > 0) {
+                sb.append(" ");
+            }
+            sb.append(termAttr.term());
+        }
+        return sb.toString();
     }
 }
 
--- JavaIndexer/src/main/Main.java
+++ JavaIndexer/src/main/Main.java
@@ -5,16 +5,43 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+<<<<<<< Updated upstream
+=======
+        /*
+        String query = "ref:\"fiebre oro\"~2 AND ref:\"rio nilo\"~2";
+
+        String[] terms = query.split("\"", 0);
+
+        for (String term : terms) {
+            System.out.println(term);
+        }
+
+>>>>>>> Stashed changes
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
         String word = "cuanacaste~0.8";
         int sep = word.indexOf('~');
 
+<<<<<<< Updated upstream
         System.out.println(word.substring(sep + 1));
 
         /*ArchiveManager index = new ArchiveManager();
         index.index_HTMLS("Asia");
         index.searchQuery();*/
+=======
+        for (String term : terms) {
+            System.out.println(term);
+        }
+*/
+        ArchiveManager index = new ArchiveManager();
+        index.creator_();
+        index.index_Files("África");
+        index.searchQuery();
+        index.index_Files("Asia");
+        index.searchQuery();
+        index.index_Files("América");
+        index.index_Files("Europa");
+>>>>>>> Stashed changes
     }
 
 
